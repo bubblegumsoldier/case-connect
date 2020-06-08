812ef540fca1e3674fe273ebe8cceac6d93b64e5
Henry Müssemann <henry.muessemann@solve-studios.de>
2020-06-08 12:48:30 +0200

    Added JSON output

--- caseconnect/program.py
+++ caseconnect/program.py
@@ -7,10 +7,21 @@ try:
 except ImportError:
     from yaml import Dumper
 
+import json
+
+def serialize(obj):
+    """JSON serializer for objects not serializable by default json code"""
+    return obj.__dict__
+
+
 class Program:
     def main(self):
         patient_repository = PatientRepository(initialize=True)
         similar_patients = patient_repository.retrieve_similar_patients("A")
+        output = json.dumps(similar_patients, default=serialize, ensure_ascii=False).encode('utf8')
+        text_file = open("output.json", "wb")
+        text_file.write(output)
+        text_file.close()
         output = dump(similar_patients, Dumper=Dumper, encoding='utf-8', allow_unicode=True)
         text_file = open("output.yml", "wb")
         text_file.write(output)
